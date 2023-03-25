2aabe8b41c9bd3a6d8701ef960def50734f9d148
Kyle Alexander <kbalexander17@yahoo.com>
2023-03-25 12:54:53 -0400

    Task1Complete

---
+++ screens/AboutScreen.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+const AboutScreen = () => {
+    return (
+        <ScrollView></ScrollView>
+    )
+}
+
+export default AboutScreen;
---
+++ screens/ContactScreen.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+const ContactScreen = () => {
+    return (
+        <ScrollView></ScrollView>
+    )
+}
+
+export default ContactScreen;
